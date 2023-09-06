03a8af1feeb4539276be9bbc952488f763e52881
EliseoGuanicoL <71537940+EliseoGuanicoL@users.noreply.github.com>
2023-09-05 18:58:43 -0600

    Se aplica Eureka Client

--- src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
+++ src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -1,5 +1,6 @@
 package com.formacionbdi.springboot.app.item;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 	
-	@Bean("clienteRest") 
+	@Bean("clienteRest")
+	@LoadBalanced
 	public RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
 	}
